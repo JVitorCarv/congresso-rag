e48a9be947f491ca26d9ee0664ba201a57679ab8
Vitor Carvalho <vitor.velozo2003@gmail.com>
2025-06-07 20:35:42 -0300

    Reduce indentation froom main.py

--- main.py
+++ main.py
@@ -8,31 +8,33 @@ def main():
     st.title("Congresso RAG")
 
     uploaded_file = st.file_uploader("Faça upload do PDF:", type="pdf")
+    if not uploaded_file:
+        return
+
     query = st.text_input("Digite sua pergunta:")
 
-    if uploaded_file:
-        with st.spinner("Carregando documento..."):
-            text_preprocessor = TextPreprocessor(uploaded_file)
+    with st.spinner("Carregando documento..."):
+        text_preprocessor = TextPreprocessor(uploaded_file)
+        docs = text_preprocessor.load_file()
+        st.success(f"{len(docs)} páginas do PDF foram carregadas.")
 
-            docs = text_preprocessor.load_file()
-            st.success(f"{len(docs)} páginas do PDF foram carregadas.")
+    with st.spinner("Carregando embeddings..."):
+        vector_store = text_preprocessor.load_embeddings()
+    st.success("Documento indexado.")
 
-            with st.spinner("Carregando embeddings..."):
-                vector_store = text_preprocessor.load_embeddings()
+    if not query:
+        return
 
-            st.success("Documento indexado.")
+    with st.spinner("Buscando resposta..."):
+        instructions_path = os.path.join(os.getcwd(), "instructions.txt")
+        rag = RAG(vector_store, instructions_path)
 
-            if query:
-                with st.spinner("Buscando resposta..."):
-                    instructions_path = os.path.join(os.getcwd(), "instructions.txt")
-                    rag = RAG(vector_store, instructions_path)
-
-                    try:
-                        response = rag.get_response(query)
-                        st.subheader("Resposta:")
-                        st.write(response.output_text)
-                    except Exception as e:
-                        st.error(f"Erro ao gerar resposta: {e}")
+        try:
+            response = rag.get_response(query)
+            st.subheader("Resposta:")
+            st.write(response.output_text)
+        except Exception as e:
+            st.error(f"Erro ao gerar resposta: {e}")
 
 
 if __name__ == "__main__":
